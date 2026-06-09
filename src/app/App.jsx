61c00691567ed526f73869a3a99f1773f442f480
harcharan <harcharandasari@gmail.com>
2026-06-09 20:26:15 +0530

    Completed Speakathon changes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
           <Route path="/ideathon" element={<IdeaThonPage />} />
           <Route path="/game-a-thon" element={<GameAThonPage />} />
           <Route path="/speak-a-thon" element={<SpeakAThonPage />} />
+          <Route path="/speakathon" element={<SpeakAThonPage />} />
           <Route path="/design-a-thon" element={<DesignAThonPage />} />
         </Routes>
       </div>
